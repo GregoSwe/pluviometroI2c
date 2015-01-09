f11c916ac6d349aa82e5ff3867ac0c9fbd876ff3
Grego <marco.grego@hotmail.it>
2015-01-09 12:56:39 +0100

    aggiunta funzione per cancellare la memoria dello slave

--- PluviometroI2C_Master/SlaveInterface.cpp
+++ PluviometroI2C_Master/SlaveInterface.cpp
@@ -4,7 +4,12 @@
     byte* rawByte = SCH.getNewTimestamp(slaveAddress,false);
     int timestamps = SCH.requireNewTimestamp(slaveAddress)/6; // requireNewTimestamp ritorna il numero di byte che sono richiesti per i timestamp, dividendo per 6 ho il numero di timestamp
     return TimestampContainer(rawByte,timestamps);
-}
+ }
+
+ int SlaveInterfaceClass::resetMemory()
+ {
+     return SCH.requireDeleteMemory(slaveAddress);
+ }
 
 SlaveInterfaceClass::SlaveInterfaceClass(byte _add):
     slaveAddress(_add)
--- PluviometroI2C_Master/SlaveInterface.h
+++ PluviometroI2C_Master/SlaveInterface.h
@@ -11,7 +11,8 @@ private:
     SlaveComunicationHandler SCH;
     byte slaveAddress;
 public:
-    TimestampContainer requireTimestamp();
+    TimestampContainer requireTimestamp(); // ritorna un TimestampContainer con tutti i timestamp non ancora letti
+    int resetMemory(); // richiede la cancellazione della memoriadella scheda slave il ripristino dei contatori, ritorna il numero di byte che sono stati cancellati
     SlaveInterfaceClass(byte _add);
     void setSlaveAddress(int _newAddress);
 };
