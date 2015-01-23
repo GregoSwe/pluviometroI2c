278b5a77f3e17e756c5157ec7a8689b5198c5662
Grego <marco.grego@hotmail.it>
2015-01-23 09:39:35 +0100

    Rimossi JSonConverter

--- PluviometroI2C_Master/SlaveInterface.cpp
+++ PluviometroI2C_Master/SlaveInterface.cpp
@@ -1,8 +1,10 @@
 #include "SlaveInterface.h"
  TimestampContainer SlaveInterfaceClass::requireTimestamp()
 {
-    byte* rawByte = SCH.getNewTimestamp(slaveAddress,false);
-    int timestamps = SCH.requireNewTimestamp(slaveAddress)/6; // requireNewTimestamp ritorna il numero di byte che sono richiesti per i timestamp, dividendo per 6 ho il numero di timestamp
+
+   int timestamps = SCH.requireNewTimestamp(slaveAddress)/6; // requireNewTimestamp ritorna il numero di byte che sono richiesti per i timestamp, dividendo per 6 ho il numero di timestamp
+   byte* rawByte = SCH.getNewTimestamp(slaveAddress,false);
+
     return TimestampContainer(rawByte,timestamps);
  }
 
